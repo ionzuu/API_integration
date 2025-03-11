95d3f50ea621b95016d66a3ac36492f6dc5d24e3
ionzuu <jonathan.costilla@protonmail.com>
2025-03-10 21:17:31 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,80 @@
+'use-strict';
+
+locations = [
+    {
+        id: 'mx-nln',
+        name: 'Nuevo Leon, Mexico',
+        lat: 25.686613,
+        lon: -100.316116,
+    },
+    {
+        id: 'us-al-tx',
+        name: 'Arlington, Texas, United States',
+        lat: 32.705002,
+        lon: -97.122780,
+    },
+    {
+        id: 'jp-osk',
+        name: 'Osaka, Japan',
+        lat: 34.6937378,
+        lon: 135.50216509999996,
+    },
+]
+
+
+const send = document.getElementById('btnset');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+
+send.addEventListener('click', (e)=>{
+    e.preventDefault();
+    const location = document.getElementById('location').value;
+    location !== '' ? openModal(location) : alert('Select a correct location.');
+    
+});
+
+let getInfo;
+
+function openModal(location){
+    console.log(location)
+    // Show modal
+    modal.classList.remove('hidden');
+    modal.classList.add('show');
+    // Active an overlay
+    overlay.classList.remove('hidden');
+    overlay.classList.add('show');
+    getData(location)
+}
+
+async function getData(location) {
+    currentLocation = {};
+    currentLocation = locations.find((locat) => locat.id === location)
+    console.log(currentLocation)
+    try {
+        const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${currentLocation?.lat}&longitude=${currentLocation?.lon}&current=temperature_2m,wind_speed_10m&current=&timezone=auto`);
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return showInfo( await response.json(), currentLocation);
+    } catch (error) {
+        console.error("Error:", error);
+    }
+}
+
+
+function showInfo(getInfo, currentLocation){
+    const header = document.getElementById('header');
+    const main = document.getElementById('main');
+    const footer = document.getElementById('footer');
+    console.log(getInfo)
+    header.innerHTML = `<h1 class='title-header'>${currentLocation.name}</h1><br><h3 class='title-header'>Current Time: ${getInfo?.current.time.replace('T', ' ')}</h3>`
+    main.innerHTML = `  
+        <div class="temp">Temperature: ${getInfo?.current.temperature_2m}°</div><div class="locate">Latitude: ${currentLocation.lat} <br> Longitude: ${currentLocation.lon}</div>
+        <div class="wind">Wind speed: ${getInfo?.current.wind_speed_10m} mph</div>
+    `
+}
+
+// id: 'mx-nln',
+// name: 'Nuevo Leon, Mexico',
+// lat: 25.686613,
+// lon: -100.316116,
